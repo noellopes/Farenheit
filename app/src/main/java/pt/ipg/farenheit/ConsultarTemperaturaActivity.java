84b97465e679d268f53a5e263721815f43920635
Noel Lopes <noel@ipg.pt>
2020-11-06 20:05:15 +0000

    Primeira versão funcional Uso de uma classe estática para passar informações

--- app/src/main/java/pt/ipg/farenheit/ConsultarTemperaturaActivity.java
+++ app/src/main/java/pt/ipg/farenheit/ConsultarTemperaturaActivity.java
@@ -7,16 +7,15 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class ConsultarTemperaturaActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultar_temperatura);
 
-        Intent intent = getIntent();
-        double temperatura = intent.getDoubleExtra(MainActivity.TEMPERATURA, 0.0);
-
         TextView textViewCelsius = findViewById(R.id.textViewCelsius);
-        textViewCelsius.setText(String.valueOf(temperatura));
+        textViewCelsius.setText(String.valueOf(DadosApp.temperatura.GetCelsius()));
+
+        TextView textViewFahrenheit = findViewById(R.id.textViewFahrenheit);
+        textViewFahrenheit.setText(String.valueOf(DadosApp.temperatura.GetFahrenheit()));
     }
 }
--- app/src/main/java/pt/ipg/farenheit/MainActivity.java
+++ app/src/main/java/pt/ipg/farenheit/MainActivity.java
@@ -6,11 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final String TEMPERATURA = "pt.ipg.farenheit.TEMPERATURA";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         EditText editTextTemperatura = findViewById(R.id.editTextTemperatura);
         String s = editTextTemperatura.getText().toString();
 
+        RadioButton radioButtonCelcius = findViewById(R.id.radioButtonCelcius);
+
         double temperatura;
         try {
             temperatura = Double.parseDouble(s);
@@ -29,8 +29,13 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
+        if (radioButtonCelcius.isChecked()) {
+            DadosApp.temperatura = new TemperaturaCelsius(temperatura);
+        } else {
+            DadosApp.temperatura = new TemperaturaFahrenheit(temperatura);
+        }
+
         Intent intent = new Intent(this, ConsultarTemperaturaActivity.class);
-        intent.putExtra(TEMPERATURA, temperatura);
         startActivity(intent);
     }
 }
