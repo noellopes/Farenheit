73cb0ba9d808ea7b5a6116bc463f4ab8298c8c75
Noel Lopes <noel@ipg.pt>
2020-11-06 20:53:07 +0000

    Formatar as temperaturas com duas casas decimais

--- app/src/main/java/pt/ipg/farenheit/ConsultarTemperaturaActivity.java
+++ app/src/main/java/pt/ipg/farenheit/ConsultarTemperaturaActivity.java
@@ -6,16 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 public class ConsultarTemperaturaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultar_temperatura);
 
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
         TextView textViewCelsius = findViewById(R.id.textViewCelsius);
-        textViewCelsius.setText(String.valueOf(DadosApp.temperatura.GetCelsius()));
+        textViewCelsius.setText(
+                decimalFormat.format(DadosApp.temperatura.GetCelsius())
+        );
 
         TextView textViewFahrenheit = findViewById(R.id.textViewFahrenheit);
-        textViewFahrenheit.setText(String.valueOf(DadosApp.temperatura.GetFahrenheit()));
+        textViewFahrenheit.setText(
+                decimalFormat.format(DadosApp.temperatura.GetFahrenheit())
+        );
     }
 }
